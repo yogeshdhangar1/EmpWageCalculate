6bc490698171738483d25d6ae7599ac08c28179d
yogeshdhangar1 <yogeshdhangar5213@gmail.com>
2022-07-06 06:15:14 -0700

    Added: Month Wage calculate

--- src/com/Bridgelabz/EmpWageBuilder.java
+++ src/com/Bridgelabz/EmpWageBuilder.java
@@ -4,26 +4,34 @@ public class EmpWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Emp Wage Builder");
-		int isPartTime = 1 , isFullTime = 2 ;
-		int WagePerHour = 20 ;
-		int empHrs = 0 , empDailyWage = 0;
-		int empCheck = (int)Math.floor ( Math.random() * 10 ) % 3 ;
-		switch (empCheck)
-		{
-		case 1 :
-			empHrs=4;
-			empDailyWage = (empHrs*WagePerHour);
-			System.out.println("Employee is Presnet :-" + empDailyWage);
-			break;
-		case 2 :
-			empHrs=8;
-			empDailyWage = (empHrs*WagePerHour);
-			System.out.println("Employee is Present :-" + empDailyWage);
-			break;
-		default:
-			empHrs=0;
-			System.out.println("Employee is Absent :- " +empDailyWage);
-			break;	
+		int isPartTime=1;
+		int isFullTime=2;
+		int WagePerHr=20;
+		int empHrs=0;
+		int totalEmpHrs=0;
+		int empWage=0;
+		int numOfWorkingDays=20;
+		int EmpRatePerHr = 2;
+		int totalEmpWage = 0;
+		//loop condition 
+
+		for (int i=0; i < numOfWorkingDays; i++ ) {
+			int empCheck = (int)Math.floor (Math.random() * 10 ) % 3;
+			switch (empCheck) {
+			case 1 :
+				empHrs=4;
+				break;
+			case 2 :
+				empHrs=8;
+				break;
+			default:
+				empHrs=0;
+				break;
+			}
+			empWage = empHrs * EmpRatePerHr;
+			totalEmpHrs = totalEmpHrs+empHrs;
+			System.out.println("Emp Wage:  " +empWage);
 		}
+		System.out.println("Employee Wage For the Month:- " +totalEmpWage);
 	}
 }
