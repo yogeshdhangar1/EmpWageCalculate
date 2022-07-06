e32ac6cea1ccc4bc633cf460313b06c7f8ef68e8
yogeshdhangar1 <yogeshdhangar5213@gmail.com>
2022-07-06 05:18:31 -0700

    Added: Attendence Check

--- src/com/Bridgelabz/EmpWageBuilder.java
+++ src/com/Bridgelabz/EmpWageBuilder.java
@@ -4,7 +4,17 @@ public class EmpWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Emp Wage Builder");
+		int IS_FULL_TIME=1;
+		double empCheck= Math.floor(Math.random() * 10) % 2 ;
+		if (empCheck== IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.err.println("Employee is Absent");
 
 	}
 
+
+
 }
+
+
