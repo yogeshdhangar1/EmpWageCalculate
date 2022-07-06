b0162469bf69f155d8480fb393da1aeaa73627d4
yogeshdhangar1 <yogeshdhangar5213@gmail.com>
2022-07-06 05:27:45 -0700

    Added:Part Time Wage calculation

--- src/com/Bridgelabz/EmpWageBuilder.java
+++ src/com/Bridgelabz/EmpWageBuilder.java
@@ -4,16 +4,25 @@ public class EmpWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Emp Wage Builder");
-		int isFullTime=1;
-		int WagePerHour=20;
-		int  empHrs = 0 , empDailyWage = 0 ;
-		double empCheck = Math.floor(Math.random () * 10 ) % 2 ;
-		if ( empCheck==isFullTime) {
-			empHrs = 8 ;
+		int isPartTime =1 ,  isFullTime =2 ;
+		int WagePerHour = 20;
+		//Variables
+		int empHrs = 0 , empDailyWage = 0;
+		double empCheck = Math. floor (Math.random()* 10 ) % 3 ;
+		if ( empCheck == isPartTime ) {
+			empHrs=4;
 			empDailyWage = (empHrs*WagePerHour);
-			System.out.println(" Employee is Present :-" + empDailyWage );
-		} else {
-			System.err.println(" Employee is Absent :-" + empDailyWage );
+			System.out.println("Employee is Present :- " + empDailyWage );
 		}
+		else 
+			if ( empCheck == isFullTime ) {
+				empHrs = 8;
+				empDailyWage = (empHrs*WagePerHour);
+				System.out.println("Employee is Present : - " + empDailyWage );
+			}
+			else {
+				empHrs=0;
+				System.out.println("Employee is Absent :-" + empDailyWage);
+			}
 	}
 }
