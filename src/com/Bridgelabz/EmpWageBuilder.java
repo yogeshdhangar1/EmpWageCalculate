babd26a53015d9e6df91289cc6fdce3a6969a72c
yogeshdhangar1 <yogeshdhangar5213@gmail.com>
2022-07-06 06:24:45 -0700

    Added Wage Calculate

--- src/com/Bridgelabz/EmpWageBuilder.java
+++ src/com/Bridgelabz/EmpWageBuilder.java
@@ -2,36 +2,39 @@ package com.Bridgelabz;
 
 public class EmpWageBuilder {
 
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20 ;
+	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_RATE_IN_MONTH = 10 ;
 	public static void main(String[] args) {
-		System.out.println("Welcome to Emp Wage Builder");
-		int isPartTime=1;
-		int isFullTime=2;
-		int WagePerHr=20;
-		int empHrs=0;
-		int totalEmpHrs=0;
-		int empWage=0;
-		int numOfWorkingDays=20;
-		int EmpRatePerHr = 2;
-		int totalEmpWage = 0;
-		//loop condition 
+		//Variables
+		int empHrs = 0 , totalEmpHrs = 0 , totalWorkingDays = 0 ;
+		//computation 
+		while (totalEmpHrs <= MAX_RATE_IN_MONTH && 
+				totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 
-		for (int i=0; i < numOfWorkingDays; i++ ) {
-			int empCheck = (int)Math.floor (Math.random() * 10 ) % 3;
+			int empCheck = (int) Math.floor (Math.random () * 10 ) % 3 ;
 			switch (empCheck) {
-			case 1 :
+			case IS_PART_TIME:
 				empHrs=4;
 				break;
-			case 2 :
+			case IS_FULL_TIME:
 				empHrs=8;
 				break;
 			default:
 				empHrs=0;
-				break;
+
 			}
-			empWage = empHrs * EmpRatePerHr;
-			totalEmpHrs = totalEmpHrs+empHrs;
-			System.out.println("Emp Wage:  " +empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day#:" + totalWorkingDays +" Emp Hr:+ " +empHrs);
+
+
 		}
-		System.out.println("Employee Wage For the Month:- " +totalEmpWage);
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR ;
+		System.out.println("total Emp Wage :" +totalEmpWage  );
 	}
+
 }
+
