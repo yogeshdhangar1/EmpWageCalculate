e5143b51dcfcddf96ef2e8ffe54c5beae71ad09d
yogeshdhangar1 <yogeshdhangar5213@gmail.com>
2022-07-06 05:24:04 -0700

    Added: Daily Wage Calculate

--- src/com/Bridgelabz/EmpWageBuilder.java
+++ src/com/Bridgelabz/EmpWageBuilder.java
@@ -4,17 +4,16 @@ public class EmpWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Emp Wage Builder");
-		int IS_FULL_TIME=1;
-		double empCheck= Math.floor(Math.random() * 10) % 2 ;
-		if (empCheck== IS_FULL_TIME)
-			System.out.println("Employee is Present");
-		else
-			System.err.println("Employee is Absent");
-
+		int isFullTime=1;
+		int WagePerHour=20;
+		int  empHrs = 0 , empDailyWage = 0 ;
+		double empCheck = Math.floor(Math.random () * 10 ) % 2 ;
+		if ( empCheck==isFullTime) {
+			empHrs = 8 ;
+			empDailyWage = (empHrs*WagePerHour);
+			System.out.println(" Employee is Present :-" + empDailyWage );
+		} else {
+			System.err.println(" Employee is Absent :-" + empDailyWage );
+		}
 	}
-
-
-
 }
-
-
